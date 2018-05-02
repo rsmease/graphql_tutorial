a177364bb7f84a0873f979664ce767ae1963c028
Ryan <rmease@lumoslabs.com>
2018-05-02 16:38:03 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+  hello: () => {
+    return 'Hello World!';
+  }
+};
+
+const app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
